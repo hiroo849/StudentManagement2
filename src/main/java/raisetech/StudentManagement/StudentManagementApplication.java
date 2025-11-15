50102012fb873921ea39d0236f6dacc9911c35c2
hiroo849 <hoom004@softbank.ne.jp>
2025-11-15 11:07:24 +0900

    追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,18 +3,26 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+  public  String  name  = "Enami kouji";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
- @GetMapping("/hello")
-  public String hello(){
-    return "Hello, world!";
+ @GetMapping("/name")
+  public String getName() {
+    return name;
+  }
+
+  @PostMapping("/name")
+  public void  setName(String name){
+    this.name = name;
   }
 }
