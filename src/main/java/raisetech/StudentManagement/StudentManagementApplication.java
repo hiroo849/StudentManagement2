5e8a1f9a799ab1406c202afdb08d61fa8be6a0a3
hiroo849 <hoom004@softbank.ne.jp>
2025-11-28 12:03:53 +0900

    第20回授業。Read処理のServiceとController部分を実装。

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -6,25 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.repository.StudentRepository;
 
 @SpringBootApplication
-@RestController
 public class StudentManagementApplication {
-
-  @Autowired
-  private StudentRepository repository;
 
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
-  }
 
-  @GetMapping("/studentList")
-  public List<Student> getStudent(){
-    return repository.search();
-  }
-
-  @GetMapping("/studentsCourseList")
-  public List<StudentsCourses> getStudentsCourseList(){
-    return repository.searchStudentsCourses();
   }
 }
