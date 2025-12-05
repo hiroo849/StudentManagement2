45f989f6189edb0496ffa14470e45305eb6e22f5
hiroo849 <hoom004@softbank.ne.jp>
2025-12-05 17:45:24 +0900

    第25回授業。第14回演習課題まで。

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package raisetech.StudentManagement.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
@@ -9,16 +11,16 @@ import raisetech.StudentManagement.data.StudentsCourses;
 /**
  * 受講生情報を扱うリポジトリ。
  *
- * 全権検索や単一条件での検索、コース情報の検索が行えるクラスです。
+ * 全件検索や単一条件での検索、コース情報の検索が行えるクラスです。
  */
 
 @Mapper
 public interface StudentRepository {
 
   /**
-   * 全権検索します。
+   * 全件検索します。
    *
-   * @return　全権検索した受講生情報の一覧
+   * @return　全件検索した受講生情報の一覧
    */
 
   @Select("SELECT * FROM students")
@@ -26,4 +28,10 @@ public interface StudentRepository {
 
   @Select("SELECT * FROM students_courses")
   List<StudentsCourses> searchStudentsCourses();
+
+  @Insert("INSERT INTO students(full_name, furigana, nickname, email, city, age, gender, remark, is_deleted) "
+      + "VALUES(#{fullName}, #{furigana}, #{nickname}, #{email}, #{city}, #{age}, #{gender}, #{remark}, false)")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+
+  void registerStudent(Student student);
 }
--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -3,8 +3,10 @@ package raisetech.StudentManagement.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.repository.StudentRepository;
 
 @Service
@@ -26,5 +28,10 @@ public class StudentService {
 
   public List<StudentsCourses> searchStudentsCourseList(){
     return repository.searchStudentsCourses();
+  }
+
+  @Transactional
+  public void registerStudent(StudentDetail studentDetail){
+    repository.registerStudent(studentDetail.getStudent());
   }
 }
